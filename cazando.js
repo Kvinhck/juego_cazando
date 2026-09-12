0a3c4e0f547b5635290cf9c9a4d562d8d5b55ff3
Kvinhck <kevinverac4@gmail.com>
2026-09-11 20:37:05 -0500

    variables y constantes declaradas

--- cazando.js
+++ cazando.js
@@ -1,7 +1,26 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
+let gatoX=canvas.width/2;
+let gatoY=canvas.height/2;
+let comidaX=0;
+let comidaY=canvas.height;
+const ALTO_GATO=65;
+const ANCHO_GATO=40;
+const ALTO_COMIDA=20;
+const ANCHO_COMIDA=20;
+
+
+function iniciarJuego(){
+    graficarGato();
+    graficarComida();   
+}
 
 function graficarGato(){
     ctx.fillStyle="red";
-    ctx.fillRect(canvas.width/2,canvas.height/2,20,40);
+    ctx.fillRect(gatoX,gatoY,ANCHO_GATO,ALTO_GATO);
+}
+
+function graficarComida(){
+    ctx.fillStyle="green";
+    ctx.fillRect(comidaX,comidaY-ALTO_COMIDA,ANCHO_COMIDA,ALTO_COMIDA);
 }
